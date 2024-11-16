3c8119d8772487030ea6527d3d51070345f94fdf
NanaKhodes <ekwerenancy266@gmail.com>
2024-11-16 19:36:14 +0100

    fixed firebase config

--- src/authentication/firebase.js
+++ src/authentication/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "movie-app-ae6ba.firebaseapp.com",
   projectId: "movie-app-ae6ba",
   storageBucket: "movie-app-ae6ba.firebasestorage.app",
@@ -10,6 +10,7 @@ const firebaseConfig = {
   appId: "1:868391841100:web:372e4f10890b201dcdf3e9",
   measurementId: "G-1JFX9943CX",
 };
+
 
 
 const app = initializeApp(firebaseConfig);
